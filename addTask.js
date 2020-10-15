ba63494498431bd28ef15fddb0a6b3d2910c1644
masa <masa28.develop@gmail.com>
2020-10-16 07:35:32 +0900

    fix display task list logic

--- addTask.js
+++ addTask.js
@@ -1,42 +1,51 @@
 // call function
 const displayTaskList = function(taskList) {
     const elem = document.getElementById('task-list');
-    let newTable;
 
-    for (i = 0; i < taskList.length; i++) {
-        const taskTemplate = `
-        <tr>
-            <td>${i}</td>
-            <td>${taskList[i]['comment']}</td>
-            <td>
-                <button>${taskList[i]['status']}</button>
-                <button>削除</button>
-            </td>
-        </tr>`
-
-        if (i === 0) {
-            newTable = taskTemplate
-        } else {
-            newTable = newTable + taskTemplate
-        }
-        elem.innerHTML = newTable;
+    // Delete previous displayed task list
+    while (elem.lastChild) {
+        elem.removeChild(elem.lastChild);
     }
-}
+
+    // Display task list
+    taskList.forEach(function(task) {
+        const tableRow = document.createElement('tr');
+        const idData = document.createElement('td');
+        const commentData = document.createElement('td');
+        const statusData = document.createElement('td');
+        const progressButton = document.createElement('button');
+        const deleteButton = document.createElement('button');
+
+        deleteButton.textContent = '削除';
+        idData.textContent = task['id'];
+        commentData.textContent = task['comment'];
+        progressButton.textContent = task['status'];
+
+        statusData.appendChild(progressButton);
+        statusData.appendChild(deleteButton);
+
+        tableRow.appendChild(idData);
+        tableRow.appendChild(commentData);
+        tableRow.appendChild(statusData);
+
+        elem.appendChild(tableRow);
+    });
+};
 
 const addTaskList = function(taskList) {
     let comment = document.getElementById('comment');
     const task = {
         comment: comment.value,
         status: '作業中',
-    }
+    };
     comment.value = '';
     taskList.push(task);
-}
+};
 
 // main Logic
 const taskList = [];
 
-function clickBtn() {
+function addTaskButton() {
     addTaskList(taskList);
     displayTaskList(taskList);
 }
